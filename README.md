aaa9137f9e2800f64c004049b9951a5e9f756e77
shahart <shahar_t@hotmail.com>
2020-09-01 12:11:40 +0300

    the basic - show only white list of contacts

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # WhatsHide
-This extension hides private chats of WhatsApp Web
+
+a private fork of an [extension](https://chrome.google.com/webstore/detail/whatshide/hooppjkiofnpgbmokmcpppihhjdmjlbk) to hide private chats of WhatsApp Web
--- scripts/filterdom.js
+++ scripts/filterdom.js
@@ -6,39 +6,15 @@
 setTimeout(initExecution, 1000); //Init execution of the process all chats
 
 /**
- * Strip special caracter of string
- *  @memberof String
- */
-String.prototype.strip = function() {
-    return this
-        .replace(/[áàãâä]/gi, "a")
-        .replace(/[éè¨ê]/gi, "e")
-        .replace(/[íìïî]/gi, "i")
-        .replace(/[óòöôõ]/gi, "o")
-        .replace(/[úùüû]/gi, "u")
-        .replace(/[ç]/gi, "c")
-        .replace(/[ñ]/gi, "n")
-        .replace(/[^a-zA-Z0-9]/g, " ");
-}
-
-/**
- * Veirfy if a specific string contains any substring in subtrings param
- *  @param {Array.<string>} subtrings Substrings Array to check if contains in string
+ * Verify if a specific string startsWith any substring in subtrings param
+ *  @param {Array.<string>} subtrings Substrings Array to check if startsWith in string
  *  @memberof String
  */
 String.prototype.containsAny = function(substrings) {
-    
-    for (var i = 0; i != substrings.length; i++) {
-        var substring = substrings[i].toLowerCase().strip().split(" ");
-        var containsSub = true;
-        for (var j = 0; j < substring.length; j++) {
-            if (this.indexOf(substring[j]) == -1) {
-                containsSub = false;                
-                break;
-            }
-        }
-        if (containsSub) {
-            return containsSub;
+  
+    for (var i = 0; i < substrings.length; i++) {
+        if (this.startsWith(substrings[i])) {
+            return true;                
         }
     }
     return false;
@@ -50,47 +26,58 @@ String.prototype.containsAny = function(substrings) {
  */
 function processChat(index) {
     var el = $(this);
-    var str = el.html();
+    var str = el.text();
 
-    if (str.toLowerCase().strip().containsAny(substrings)) {
-        el.hide();
-    } else {
+    if (str.toLowerCase().containsAny(substrings)) {
         el.show();
+    } else {
+        el.hide();
     }
 }
 
 /**
- * Var with the substrings of the storage
+ * Var with the white list of contacts
  */
 var substrings;
 
-/**
- * On get value of the current Storage
- *  @param {Array.<Object>} item Arrays of Words in the storage
- */
-function onGetValue(item) {
+function onGetValue() {
 
-    substrings = [];
-    if (item && item.words && Object.prototype.toString.call(item.words) === '[object Array]') {
-        for (var i = 0; i < item.words.length; i++) {
-            if (item.words[i].active) {
-                substrings = substrings.concat(item.words[i].value);
-            }
-        }
-    }
+	// hide header (profile, settings, ..)
+	$(document.querySelectorAll("._1QUKR")).hide();
+	// hide contact's Kebab menu - but user still can delete
+	$(document.querySelectorAll(".PVMjB")).hide();
+	// hide search, as user can reveal for a fraction of sec the chats
+	$(document.querySelectorAll("._2EoyP")).hide();	
+	// hide delete contact
+	$(document.querySelectorAll("._3wAoe")).hide();
+	// hide delete message/ forward/ etc.
+	$(document.querySelectorAll("._2s_eZ")).hide();	
+
     //Get All chats
-    var itens = document.querySelectorAll("._2wP_Y");
-
-    //Process all chats
+	var itens = document.querySelectorAll(".eJ0yJ");
+	
+	//Process all chats
     $(itens).each(processChat);
-
+		
     //Continue the execution
-    setTimeout(initExecution, 1000);
+    setTimeout(onGetValue, 250);
 }
+
 
 /**
  * init Execution
  */
 function initExecution() {
-    chrome.storage.sync.get('words', onGetValue);
+	substrings = [
+		'+972 10-123-4567', // Alice
+		'bob'
+	];
+
+	for (var i = 0; i < substrings.length; i++) {
+        if (substrings[i].toLowerCase() != substrings[i]) {
+			alert(substrings[i] + ' - not lowered case');
+		}
+	}
+
+	onGetValue();
 }
